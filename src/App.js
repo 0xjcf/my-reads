6b7fb04688cbb42e6061bfcfe0554298bca1d965
JCFlores <jflores717@gmail.com>
2018-07-01 20:23:05 -0500

    Change to Shelf and Book components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
-import CurrentlyReading from "./CurrentlyReading";
-import WantToRead from "./WantToRead";
-import Read from "./Read";
+import Shelf from './Shelf';
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState(() => ({ books }));
+    });
+  }
 
   render() {
     return (
@@ -19,14 +25,6 @@ class BooksApp extends React.Component {
               <div className="search-books-bar">
                 <Link className="close-search" to="/" />
                 <div className="search-books-input-wrapper">
-                  {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                   <input type="text" placeholder="Search by title or author" />
                 </div>
               </div>
@@ -45,9 +43,9 @@ class BooksApp extends React.Component {
                 <h1>MyReads</h1>
               </div>
               <div className="list-books-content">
-                <CurrentlyReading />
-                <WantToRead />
-                <Read />
+                <Shelf title="Currently Reading"/>
+                <Shelf title="Want To Read"/>
+                <Shelf title="Read"/>
               </div>
               <div className="open-search">
                 <Link to="/search" />

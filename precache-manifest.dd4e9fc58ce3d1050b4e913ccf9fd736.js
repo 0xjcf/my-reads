7ad9f47ebf79cfe45165e4d06f588bbad8d5b9ae
JCFlores <jflores717@gmail.com>
2018-12-17 19:44:42 -0600

    Updates

---
+++ precache-manifest.dd4e9fc58ce3d1050b4e913ccf9fd736.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "095351b8eceedd7593eb",
+    "url": "/my-reads/static/js/main.095351b8.chunk.js"
+  },
+  {
+    "revision": "88177022143b61498062",
+    "url": "/my-reads/static/js/1.88177022.chunk.js"
+  },
+  {
+    "revision": "095351b8eceedd7593eb",
+    "url": "/my-reads/static/css/main.3be97788.chunk.css"
+  },
+  {
+    "revision": "71b7de9c17e226264c529ffcf849812d",
+    "url": "/my-reads/index.html"
+  }
+];
